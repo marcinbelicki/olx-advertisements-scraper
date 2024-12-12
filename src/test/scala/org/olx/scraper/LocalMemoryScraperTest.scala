eb6db3be8759344fe87415b03ddd8e8911177643
Marcin Belicki <marcin.belicki@protonmail.com>
2024-12-12 21:40:25 +0100

    making val private

--- src/test/scala/org/olx/scraper/LocalMemoryScraperTest.scala
+++ src/test/scala/org/olx/scraper/LocalMemoryScraperTest.scala
@@ -10,7 +10,7 @@ import scala.language.postfixOps
 
 class LocalMemoryScraperTest extends AnyWordSpecLike {
 
-  val localMemoryScraper = new LocalMemoryScraper(Scraper(url = new URL("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/wroclaw/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=2500&search%5Bfilter_float_price:to%5D=3000&search%5Bfilter_float_m:from%5D=50&search%5Bfilter_enum_rooms%5D%5B0%5D=two&search%5Bfilter_enum_rooms%5D%5B1%5D=three")))
+  private val localMemoryScraper = new LocalMemoryScraper(Scraper(url = new URL("https://www.olx.pl/nieruchomosci/mieszkania/wynajem/wroclaw/?search%5Border%5D=created_at:desc&search%5Bfilter_float_price:from%5D=2500&search%5Bfilter_float_price:to%5D=3000&search%5Bfilter_float_m:from%5D=50&search%5Bfilter_enum_rooms%5D%5B0%5D=two&search%5Bfilter_enum_rooms%5D%5B1%5D=three")))
 
   "LocalMemoryScraper" must {
     "properly return new stuff when started" in {
